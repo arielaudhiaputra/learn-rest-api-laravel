5260b898b23f14d1bd23b148f2f9764ef385dd62
amarmaulana95 <42346300+amarmaulana95@users.noreply.github.com>
2025-01-29 23:09:13 +0700

    fix

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class AdminController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AdminController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
